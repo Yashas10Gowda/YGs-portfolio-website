ec32104783e340ab2465007e2ed6db49181b1f1f
Yashas Gowda <yashasgowda270@outlook.com>
2021-04-18 19:35:25 +0530

    Changed AI-Project's Tech-Stack

--- src/routes/works.svelte
+++ src/routes/works.svelte
@@ -5,35 +5,35 @@
       client: true,
       description: `I Built an E-Commerce website for a Client who had a business idea of selling used/read books online. Many industry standard functionalities like <b>OTP verification, Online Payment Gateway, Cart, Admin Interface</b> and so on are baked in into this project. Buy-Read-Exchange-Repeat!`,
       ts: "*** Confidential ***",
-      link: "https://www.bookurbooks.in/"
+      link: "https://www.bookurbooks.in/",
     },
     {
       name: "Flash Chat",
       client: false,
       description: `A simple Real-time Chat App (indeed it's a PWA which means it can be installed on a smartphone just like a Native App), that mimics the basic functionality of a WhatsApp Group Chat(a clone to de precise) where chatrooms can be created by one and joined by others.`,
       ts: "Svelte.JS, Ionic, Firebase's Firestore",
-      link: "https://flashchat-f7942.web.app/"
+      link: "https://flashchat-f7942.web.app/",
     },
     {
       name: "Portfoio Website",
       client: false,
       description: `Developed my portfolio website using W3.CSS which then turned out to be simple and minimal, exactly how I wanted it. This site is also SEO friendly, if try searching for "Yashas Gowda Postfolio Website" it'll be ranked 1st on google. Just trying to flex my SEO skills.`,
       ts: "Sapper.JS, W3.CSS",
-      link: "https://yashasgowda.web.app/"
+      link: "https://yashasgowda.web.app/",
     },
     {
       name: "Flash Video Call",
       client: false,
       description: `An "End-to-End" Encrypted <b>"Real-Time Video Calling"</b> feature was built as an extension to the previous project(Flash Chat). So Two peers across the globe can "Video-Call" each other without giving out any of their personal info instead they share a uniquely generated number with each other.`,
       ts: "WebRTC, Svelte.JS, Firebase's Firestore, Ionic",
-      link: "https://flashchat-f7942.web.app/"
+      link: "https://flashchat-f7942.web.app/",
     },
     {
       name: "Code Connector",
       client: false,
       description: `A Social Media App for developers, (available as PWA). It connects Coders, Developers, Programmers across the world to collaborate, invent and develop. I Handled everything from creation of accounts, posts to their permissions in the backend (This is a well separated frontend and backend project.)`,
       ts: "Django, Django Rest Framework, Sapper.JS, Bootstrap",
-      link: "https://codeconnector.web.app/"
+      link: "https://codeconnector.web.app/",
     },
     {
       name: "UI Templates",
@@ -66,14 +66,14 @@
       BizLand
     </a>`,
       ts: "MaterializeCSS, JQuery",
-      link: "https://github.com/Yashas10Gowda/MaterializeCSSCourse"
+      link: "https://github.com/Yashas10Gowda/MaterializeCSSCourse",
     },
     {
       name: "UPI'ly Link'ed",
       client: false,
       description: `This Webpage is exclusively built for a mobile devices with anyone UPI App (PSP App) installed in it. And visitors can just enter an amount and pay me via UPI (thereby saving myself from happening to share that cryptic UPI-ID or QR-Code EACH AND EVERYTIME).`,
       ts: "Vanilla.JS, Google's Material Components",
-      link: "https://yashas.pythonanywhere.com/"
+      link: "https://yashas.pythonanywhere.com/",
     },
     {
       name: "Desktop Apps",
@@ -103,59 +103,20 @@
       Logger
     </a>`,
       ts: "Python, PyQt5",
-      link: "https://github.com/Yashas10Gowda/3_PyQt5_GUIApps"
+      link: "https://github.com/Yashas10Gowda/3_PyQt5_GUIApps",
     },
     {
       name: "AI-Projects",
       client: false,
       description: `Projects are :- 1)CommentSense &nbsp;2)StoQueAns &nbsp;3)WhatIsIt. Names are
     a little quirky so are these projects, do check it out. When the webpage is
-    loaded for the first time it would take a minute or less to collect the
+    loaded for the first time it would take a minute or less to collect all the
     pre-trained models from the server, so please be patient.`,
-      ts: "Tensorflow.JS, Vanilla.JS, Chota.CSS",
-      link: "https://yashas.pythonanywhere.com/ai-projects/"
-    }
+      ts: "Tensorflow.JS, UI-Kit, Vanilla.JS",
+      link: "https://yashas.pythonanywhere.com/ai-projects/",
+    },
   ];
 </script>
-
-<style>
-  h1,h2{
-    margin: 10px 0;
-  }
-  h2{
-    font-weight: 500;
-  }
-  .gg-arrow-right {
-    box-sizing: border-box;
-    position: relative;
-    display: block;
-    transform: scale(var(--ggs,1));
-    width: 22px;
-    height: 22px
-  }
-  .gg-arrow-right::after,
-  .gg-arrow-right::before {
-    content: "";
-    display: block;
-    box-sizing: border-box;
-    position: absolute;
-    right: 3px
-  }
-  .gg-arrow-right::after {
-    width: 8px;
-    height: 8px;
-    border-top: 2px solid;
-    border-right: 2px solid;
-    transform: rotate(45deg);
-    bottom: 7px
-  }
-  .gg-arrow-right::before {
-    width: 16px;
-    height: 2px;
-    bottom: 10px;
-    background: currentColor
-  }
-</style>
 
 <svelte:head>
   <title>Works | Yashas Gowda</title>
@@ -177,7 +138,8 @@
     class="worksimg w3-responsive"
     style="width:100%;max-width:300px;position: fixed;right: 100px;top: 30%;"
     src="works.svg"
-    alt="" />
+    alt=""
+  />
 </div>
 
 {#each projects as project}
@@ -200,7 +162,6 @@
               <h2>{project.name}</h2>
             </div>
           {/if}
-
         </header>
         <div class="w3-container">
           <p class="w3-large">
@@ -218,11 +179,13 @@
           class="w3-button w3-block w3-white w3-text-blue w3-ripple
           w3-hover-white w3-hover-text-blue"
           style="padding:0;padding-bottom:8px;"
-          href={project.link}>
+          href={project.link}
+        >
           View Project
           <i
             style="--ggs:0.75;display:inline-block;position:relative;top:6px;"
-            class="gg-arrow-right" />
+            class="gg-arrow-right"
+          />
         </a>
       </div>
     </div>
@@ -230,3 +193,43 @@
   </div>
   <div class="w3-margin-bottom">&nbsp;</div>
 {/each}
+
+<style>
+  h1,
+  h2 {
+    margin: 10px 0;
+  }
+  h2 {
+    font-weight: 500;
+  }
+  .gg-arrow-right {
+    box-sizing: border-box;
+    position: relative;
+    display: block;
+    transform: scale(var(--ggs, 1));
+    width: 22px;
+    height: 22px;
+  }
+  .gg-arrow-right::after,
+  .gg-arrow-right::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    right: 3px;
+  }
+  .gg-arrow-right::after {
+    width: 8px;
+    height: 8px;
+    border-top: 2px solid;
+    border-right: 2px solid;
+    transform: rotate(45deg);
+    bottom: 7px;
+  }
+  .gg-arrow-right::before {
+    width: 16px;
+    height: 2px;
+    bottom: 10px;
+    background: currentColor;
+  }
+</style>
